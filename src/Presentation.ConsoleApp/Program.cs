018c2a3ff444bf98d387d351550d61a4b13d92bc
Catiane Lima Viana <katiane20@gmail.com>
2019-04-26 07:30:00 -0300

    First commit
    Learning C# first project in c#

---
+++ src/Presentation.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Viana.Domain.Interfaces;
+
+namespace Presentation.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceBase _service;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Viana.Domain/Interfaces/IServiceBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Viana.Domain.Interfaces
+{
+    public interface IServiceBase
+    {
+        void StartService();
+        TEntity GetService<TEntity>(Func<in T,  out  TEntity>);
+    }
+}
